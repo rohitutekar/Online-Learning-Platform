e0504b940d4e9581412b71862e02fb4ace80ae2b
Rohit Utekar <rohit.utekar@mirumagency.com>
2023-01-29 20:39:47 +0530

    Initial commit

---
+++ apps/store/src/app/app.tsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { Button } from '../../../../libs/shared/ui/src';
+
+export function App() {
+  return (
+    <>
+      <h1>Welcome</h1>
+      <Button />
+    </>
+  );
+}
+
+export default App;
